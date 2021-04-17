6cc821925fab1686e44cc51dae8e64b76042abe2
depler <depler.mv@gmail.com>
2021-04-17 19:41:31 +0300

    get rid of nuget packages

--- Program.cs
+++ Program.cs
@@ -54,10 +54,9 @@ namespace wgconfig
         {
             using (var generator = new QRCodeGenerator())
             using (var data = generator.CreateQrCode(text, QRCodeGenerator.ECCLevel.L))
-            using (var code = new QRCode(data))
-            using (var bitmap = code.GetGraphic(5))
+            using (var png = new PngByteQRCode(data))
             {
-                bitmap.Save(file, ImageFormat.Png);
+                File.WriteAllBytes(file, png.GetGraphic(5));
             }
         }
 
