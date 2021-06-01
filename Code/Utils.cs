10bbaa896f3367ea1dc21782bd09e623f5a37d24
depler <depler.mv@gmail.com>
2021-06-01 17:11:33 +0300

    Curve25519

--- Code/Utils.cs
+++ Code/Utils.cs
@@ -1,11 +1,7 @@
 ﻿using QRCoder;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Wireguard.Code
 {
